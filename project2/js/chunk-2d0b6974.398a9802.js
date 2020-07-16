85ffb3dab3d94ec7ecf984799ed0054590e1e61a
bohaoi <bohao8485@163.com>
2020-07-16 10:20:47 +0800

    Sales profile

---
+++ project2/js/chunk-2d0b6974.398a9802.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b6974"],{"1e8b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"pageOne"},[e._v("pageOne")])},s=[],c={},l=c,u=t("2877"),i=Object(u["a"])(l,a,s,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0b6974.398a9802.js.map
