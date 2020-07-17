f89d03aba5f6873999139548241d3c642d0a84ef
bohaoi <bohao8485@163.com>
2020-07-17 14:21:29 +0800

    change

---
+++ project2/assets/js/chunk-2d0b2935.a8f2dd24.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b2935"],{2562:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"videoManage"},[e._v("test")])},a=[],c={},l=c,u=t("2877"),i=Object(u["a"])(l,s,a,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0b2935.a8f2dd24.js.map
---
+++ project2/assets/js/chunk-2d0b6974.250a2340.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b6974"],{"1e8b":function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"pageOne"},[e._v("test1")])},a=[],c={},l=c,u=t("2877"),i=Object(u["a"])(l,s,a,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0b6974.250a2340.js.map
